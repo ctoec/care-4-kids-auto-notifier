d55e0fd4656972e21faf71892871c96eadca9b1e
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2019-08-13 10:19:41 -0500

    Specific name for MessageGenerator and update time to last hour

--- spec/notification_manager_spec.rb
+++ spec/notification_manager_spec.rb
@@ -3,23 +3,23 @@ require 'rails_helper'
 RSpec.describe NotificationManager do
   it 'enqueues a message' do
     message_queue = spy
-    message_generator = double
-    allow(message_generator).to receive(:each)
-      .and_yield('message 1')
-    notification_manager = NotificationManager.new message_queue: message_queue, message_generator: message_generator
+    indexed_documents = double
+    allow(indexed_documents).to receive(:fetch_last_hour)
+      .and_yield('fake message 1')
+    notification_manager = NotificationManager.new message_queue: message_queue, indexed_documents: indexed_documents
     notification_manager.run
-    expect(message_queue).to have_received(:put).with('message 1')
+    expect(message_queue).to have_received(:put).with('fake message 1')
   end
 
   it 'enqueues multiple messages' do
     message_queue = spy
-    message_generator = double
-    allow(message_generator).to receive(:each)
-      .and_yield('message 1')
-      .and_yield('message 2')
-      .and_yield('message 3')
+    indexed_documents = double
+    allow(indexed_documents).to receive(:fetch_last_hour)
+      .and_yield('fake message 1')
+      .and_yield('fake message 2')
+      .and_yield('fake message 3')
   
-    notification_manager = NotificationManager.new message_queue: message_queue, message_generator: message_generator
+    notification_manager = NotificationManager.new message_queue: message_queue, indexed_documents: indexed_documents
     notification_manager.run
     expect(message_queue).to have_received(:put).exactly(3).times
   end
