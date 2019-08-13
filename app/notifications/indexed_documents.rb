1ffa2b96eb49025502bece498811ac972889cdda
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2019-08-13 10:19:41 -0500

    Specific name for MessageGenerator and update time to last hour

--- app/notifications/indexed_documents.rb
+++ app/notifications/indexed_documents.rb
@@ -2,13 +2,13 @@
 DocuclassEvent = Struct.new(:type, :caseid)
 Message = Struct.new(:text, :cellphonenumber)
 
-class MessageGenerator
+class IndexedDocuments
     def initialize(docuclass_events:)
         @docuclass_events = docuclass_events
     end
 
-    def each
-        @docuclass_events.fetch_yesterday.each do |event|
+    def fetch_last_hour
+        @docuclass_events.fetch_last_hour.each do |event|
             found_parent = Applicant.where(caseid: event.caseid).first
             yield Message.new(
                 "We have received document #{event.type}", 
--- app/notifications/notification_manager.rb
+++ app/notifications/notification_manager.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 class NotificationManager
-  def initialize(message_queue:, message_generator:)
+  def initialize(message_queue:, indexed_documents:)
     @message_queue = message_queue
-    @message_generator = message_generator
+    @indexed_documents = indexed_documents
   end
 
   def run
-    @message_generator.each { |message| @message_queue.put message }
+    @indexed_documents.fetch_last_hour { |message| @message_queue.put message }
   end
 end
