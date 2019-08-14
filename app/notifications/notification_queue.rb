c1eddaee2d2c9f6a3b5fed827f8f6b1520986a95
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2019-08-13 22:39:48 -0400

    Add notification queue tests and implementation

--- app/notifications/notification_queue.rb
+++ app/notifications/notification_queue.rb
@@ -1,3 +1,23 @@
 class NotificationQueue
-  # TODO(stub)
+  def initialize(sender, scheduler)
+    if (sender.included_modules.include?(ApplicationJob)) then
+      @sender = sender
+    else
+      raise ArgumentError.new('sender should be of type ApplicationJob')
+    end
+    if (scheduler.included_modules.include?(Scheduler)) then
+      @scheduler = scheduler
+    else
+      raise ArgumentError.new('scheduler should be of type ApplicationJob')
+    end
+  end
+
+  def put(notification_event)
+    send_time = @scheduler.getNextSendTime
+    if (send_time + 1.second < Time.now) then
+      @sender.perform_now notification_event
+    else
+      @sender.perform_later notification_event
+    end
+  end
 end
--- spec/notification_queue_spec.rb
+++ spec/notification_queue_spec.rb
@@ -1,7 +1,100 @@
 require 'rails_helper'
 
 RSpec.describe NotificationQueue do
-    it 'works' do
-      expect(true).to be true
+  describe 'when constructor is supplied objects of type ApplicationJob and Scheduler' do
+    it do
+      sender = double
+      scheduler = double
+      allow(sender).to receive(:included_modules).and_return([ApplicationJob])
+      allow(scheduler).to receive(:included_modules).and_return([Scheduler])
+      expect {
+        NotificationQueue.new(sender, scheduler)
+      }.to_not raise_error
     end
+  end
+
+  describe 'when constructor is supplied objects not of type ApplicationJob and Scheduler' do
+    it do
+      sender = double
+      scheduler = double
+      allow(sender).to receive(:included_modules).and_return([])
+      allow(scheduler).to receive(:included_modules).and_return([])
+      expect {
+        NotificationQueue.new(sender, scheduler)
+      }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe 'when constructor is supplied objects of type ApplicationJob and not of Scheduler' do
+    it do
+      sender = double
+      scheduler = double
+      allow(sender).to receive(:included_modules).and_return([ApplicationJob])
+      allow(scheduler).to receive(:included_modules).and_return([])
+      expect {
+        NotificationQueue.new(sender, scheduler)
+      }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe 'when constructor is supplied objects of type Scheduler and not of ApplicationJob' do
+    it do
+      sender = double
+      scheduler = double
+      allow(sender).to receive(:included_modules).and_return([])
+      allow(scheduler).to receive(:included_modules).and_return([Scheduler])
+      expect {
+        NotificationQueue.new(sender, scheduler)
+      }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe 'when no notifications are given' do
+    it '#perform_(later|now) is called zero times' do
+      scheduler = spy
+      sender = spy
+      allow(sender).to receive(:perform_now)
+      allow(sender).to receive(:perform_later)
+
+      notificiationQueue = NotificationQueue.new(sender, scheduler)
+      
+      expect(sender).to have_received(:perform_now).exactly(0).times
+      expect(sender).to have_received(:perform_later).exactly(0).times
+    end
+  end
+
+  describe 'when one notification is given' do
+    it '#perform_(later|now) is called at most one time' do
+      scheduler = spy
+      sender = spy
+      allow(sender).to receive(:perform_now)
+      allow(sender).to receive(:perform_later)
+      notification = spy
+
+      notificiationQueue = NotificationQueue.new(sender, scheduler)
+      notificiationQueue.put notification
+
+      expect(sender).to have_received(:perform_now).at_most(:once)
+      expect(sender).to have_received(:perform_later).at_most(:once)
+    end
+  end
+
+  describe 'when several notifications are given' do
+    it '#perform_(later|now) is called no more than the same number of times' do
+      scheduler = spy
+      sender = spy
+      allow(sender).to receive(:perform_now)
+      allow(sender).to receive(:perform_later)
+      notification = spy
+      quantity = rand(1..20)
+
+      notificiationQueue = NotificationQueue.new(sender, scheduler)
+      quantity.times do
+        notificiationQueue.put notification
+      end
+
+      expect(sender).to have_received(:perform_now).at_most(quantity).times
+      expect(sender).to have_received(:perform_later).at_most(quantity).times
+    end
+  end
 end
