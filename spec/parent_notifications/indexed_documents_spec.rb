d4397fb6c139e1465f0010eb563fc71c646a773a
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2019-08-14 08:43:19 -0500

    Update test descriptions for IndexedDocuments class

--- spec/parent_notifications/indexed_documents_spec.rb
+++ spec/parent_notifications/indexed_documents_spec.rb
@@ -1,52 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe IndexedDocuments do
-    it 'returns a notification event with the correct case id' do
-        caseid = (rand 100).to_s  
-        docuclass_events = build_docuclass_events_stub(parents: [
-            Applicant.create(caseid: caseid, cellphonenumber: '5555555555')
-        ])
-        
-        indexed_documents = IndexedDocuments.new docuclass_events: docuclass_events
-        
-        notification_events = fetch_all_last_hour indexed_documents
-        expect(notification_events.first.caseid).to eql caseid
-    end
+    describe 'fetch_last_hour' do
 
-    it 'returns a notification event with a notification id that is linked stored notification with message text' do
-        caseid = (rand 100).to_s  
-        docuclass_events = build_docuclass_events_stub(parents: [
-            Applicant.create(caseid: caseid, cellphonenumber: '5555555555')
-        ])
-        indexed_documents = IndexedDocuments.new docuclass_events: docuclass_events
-        
-        notification_events = fetch_all_last_hour indexed_documents
-        notificationid = notification_events.first.notificationid
-        stored_notification = Notification.last
-        expect(stored_notification.message_text).to eql "We have received document some doc"
-    end
+        context 'there is an event that matches a parent' do 
+            it 'returns a notification event with the correct case id' do
+                caseid = (rand 100).to_s  
+                docuclass_events = build_docuclass_events_stub(parents: [
+                    Applicant.create(caseid: caseid, cellphonenumber: '5555555555')
+                ])
+                
+                indexed_documents = IndexedDocuments.new docuclass_events: docuclass_events
+                
+                notification_events = fetch_all_last_hour indexed_documents
+                expect(notification_events.first.caseid).to eql caseid
+            end
 
-    it 'generates multiple messages for a document received in docuclass' do
-        docuclass_events = build_docuclass_events_stub(parents: [
-            Applicant.create(caseid: 'x', cellphonenumber: '5555555555'),
-            Applicant.create(caseid: 'y', cellphonenumber: '5555555555')
-        ])
-        indexed_documents = IndexedDocuments.new docuclass_events: docuclass_events
-        
-        notification_events = fetch_all_last_hour indexed_documents
-        expect(notification_events.length).to eql 2
-    end
+            it 'returns a notification event with a notification id that is linked stored notification with message text' do
+                caseid = (rand 100).to_s  
+                docuclass_events = build_docuclass_events_stub(parents: [
+                    Applicant.create(caseid: caseid, cellphonenumber: '5555555555')
+                ])
+                indexed_documents = IndexedDocuments.new docuclass_events: docuclass_events
+                
+                notification_events = fetch_all_last_hour indexed_documents
+                notificationid = notification_events.first.notificationid
+                stored_notification = Notification.last
+                expect(stored_notification.message_text).to eql "We have received document some doc"
+            end
+        end
 
-    it 'does not create message if parent is not the application database' do
-        Applicant.create(caseid: 'x', cellphonenumber: '5555555555')
-        docuclass_events = build_docuclass_events_stub(parents: [
-            Applicant.create(caseid: 'y', cellphonenumber: '5555555555')
-        ])
+        context 'there are multiple events that have corresponding parents' do 
+            it 'returns all the notification events' do
+                docuclass_events = build_docuclass_events_stub(parents: [
+                    Applicant.create(caseid: 'x', cellphonenumber: '5555555555'),
+                    Applicant.create(caseid: 'y', cellphonenumber: '5555555555')
+                ])
+                indexed_documents = IndexedDocuments.new docuclass_events: docuclass_events
+                
+                notification_events = fetch_all_last_hour indexed_documents
+                expect(notification_events.length).to eql 2
+            end
+        end 
 
-        indexed_documents = IndexedDocuments.new docuclass_events: docuclass_events
+        context 'there are multiple events and some do not have a corresponding parents' do 
+            it 'returns only the notification events that correspond to a parent' do
+                Applicant.create(caseid: 'x', cellphonenumber: '5555555555')
+                docuclass_events = build_docuclass_events_stub(parents: [
+                    Applicant.create(caseid: 'y', cellphonenumber: '5555555555')
+                ])
 
-        notification_events = fetch_all_last_hour indexed_documents
-        expect(notification_events.length).to eql 1
+                indexed_documents = IndexedDocuments.new docuclass_events: docuclass_events
+
+                notification_events = fetch_all_last_hour indexed_documents
+                expect(notification_events.length).to eql 1
+            end
+        end
     end
 end
 
