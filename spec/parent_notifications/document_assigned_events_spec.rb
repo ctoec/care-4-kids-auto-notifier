502c6f69148b7fd5f5226de9f839b1ab7e3c2e4c
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2019-08-22 17:12:22 -0500

    Change fetch_new to fetch_all_new

--- spec/parent_notifications/document_assigned_events_spec.rb
+++ spec/parent_notifications/document_assigned_events_spec.rb
@@ -19,10 +19,10 @@ RSpec.describe DocumentAssignedEvents do
     @client.prepare('DELETE FROM document_assigned_index;').execute
   end
 
-  describe 'fetch_new' do
+  describe 'fetch_all_new' do
     context 'there are no events' do
       it 'returns nothing' do
-        expect(DocumentAssignedEvents.fetch_new.length).to eql 0
+        expect(DocumentAssignedEvents.fetch_all_new.length).to eql 0
       end
     end
 
@@ -34,7 +34,7 @@ RSpec.describe DocumentAssignedEvents do
         insert_command = @client.prepare(INSERT_STATEMENT)
         insert_command.execute('1234', 'doc type 1', current_time)
 
-        event = DocumentAssignedEvents.fetch_new.first
+        event = DocumentAssignedEvents.fetch_all_new.first
         expect(event.caseid).to eql '1234'
         expect(event.type).to eql 'doc type 1'
         expect(event.date.strftime('%a %b %e %T %Y')).to eql current_time.strftime('%a %b %e %T %Y')
@@ -50,31 +50,19 @@ RSpec.describe DocumentAssignedEvents do
         insert_command.execute('1234', 'doc type 1', current_time)
         insert_command.execute('1235', 'doc type 1', current_time)
 
-        expect(DocumentAssignedEvents.fetch_new.length).to eql 2
+        expect(DocumentAssignedEvents.fetch_all_new.length).to eql 2
       end
     end
 
     context 'there is are events that did not happen in the last hour' do
       it 'fetches only the events that happened after cursor date' do
-        EventCursor.document_assigned_events_cursor = Time.now
+        EventCursor.document_assigned_events_cursor =  Time.now
 
         insert_command = @client.prepare(INSERT_STATEMENT)
         insert_command.execute('1235', 'doc type 1',  Time.now - 1.hours)
         insert_command.execute('1234', 'doc type 1',  Time.now)
 
-        expect(DocumentAssignedEvents.fetch_new.length).to eql 1
-      end
-
-      it 'does not fetch events again' do
-        EventCursor.document_assigned_events_cursor = Time.now - 1.hour
-
-        insert_command = @client.prepare(INSERT_STATEMENT)
-        insert_command.execute('1235', 'doc type 1',  Time.now - 10.minutes)
-        insert_command.execute('2345', 'doc type 2',  Time.now - 20.minutes)
-        insert_command.execute('3456', 'doc type 1',  Time.now - 30.minutes)
-
-        expect(DocumentAssignedEvents.fetch_new.length).to eql 3
-        expect(DocumentAssignedEvents.fetch_new.length).to eql 0
+        expect(DocumentAssignedEvents.fetch_all_new.length).to eql 1
       end
     end
   end
