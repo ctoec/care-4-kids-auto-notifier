6c501749cc3eebbb83caea956781ed023d1d98d6
Gabriel Ramirez <gabriel.e.ramirez@gmail.com>
2019-08-22 17:38:05 -0500

    Ensure that cursor updates each time DocumentAssignedEvents.fetch_all_new runs

--- app/parent_notifications/document_assigned_events.rb
+++ app/parent_notifications/document_assigned_events.rb
@@ -14,12 +14,22 @@ class DocumentAssignedEvents
     query = <<-SQL
             SELECT *
             FROM document_assigned_index
-            WHERE index_date >= ?;
+            WHERE index_date > ?;
     SQL
-    @@client
+    events = @@client
       .prepare(query)
       .execute(EventCursor.document_assigned_events_cursor)
       .map { |row| build_event row }
+    update_cursor(events: events)
+    return events
+  end
+
+  def self.update_cursor(events:)
+    unless events.empty?
+      most_recent_event_time = events.pluck(:date).sort.last
+      EventCursor.document_assigned_events_cursor = most_recent_event_time
+    end
+    events
   end
 
   def self.build_event(row)
--- spec/parent_notifications/document_assigned_events_spec.rb
+++ spec/parent_notifications/document_assigned_events_spec.rb
@@ -28,16 +28,16 @@ RSpec.describe DocumentAssignedEvents do
 
     context 'there is one new event' do
       it 'fetches one event with the correct format' do
-        current_time = Time.now
+        EventCursor.document_assigned_events_cursor = Time.now
 
-        EventCursor.document_assigned_events_cursor = current_time
         insert_command = @client.prepare(INSERT_STATEMENT)
-        insert_command.execute('1234', 'doc type 1', current_time)
+        document_time = Time.now + 1.second
+        insert_command.execute('1234', 'doc type 1', document_time)
 
         event = DocumentAssignedEvents.fetch_all_new.first
         expect(event.caseid).to eql '1234'
         expect(event.type).to eql 'doc type 1'
-        expect(event.date.strftime('%a %b %e %T %Y')).to eql current_time.strftime('%a %b %e %T %Y')
+        expect(event.date.strftime('%a %b %e %T %Y')).to eql document_time.strftime('%a %b %e %T %Y')
       end
     end
 
@@ -47,8 +47,8 @@ RSpec.describe DocumentAssignedEvents do
         EventCursor.document_assigned_events_cursor = current_time
 
         insert_command = @client.prepare(INSERT_STATEMENT)
-        insert_command.execute('1234', 'doc type 1', current_time)
-        insert_command.execute('1235', 'doc type 1', current_time)
+        insert_command.execute('1234', 'doc type 1', current_time + 1.second)
+        insert_command.execute('1235', 'doc type 1', current_time + 2.second)
 
         expect(DocumentAssignedEvents.fetch_all_new.length).to eql 2
       end
@@ -59,10 +59,22 @@ RSpec.describe DocumentAssignedEvents do
         EventCursor.document_assigned_events_cursor = Time.now
 
         insert_command = @client.prepare(INSERT_STATEMENT)
-        insert_command.execute('1235', 'doc type 1',  Time.now - 1.hours)
-        insert_command.execute('1234', 'doc type 1',  Time.now)
+        insert_command.execute('1235', 'doc type 1',  Time.now - 1.second)
+        insert_command.execute('1234', 'doc type 1',  Time.now + 1.second)
 
         expect(DocumentAssignedEvents.fetch_all_new.length).to eql 1
+      end
+
+      it 'does not fetch events again' do
+        EventCursor.document_assigned_events_cursor = Time.now - 1.hour
+
+        insert_command = @client.prepare(INSERT_STATEMENT)
+        insert_command.execute('1235', 'doc type 1',  Time.now - 10.minutes)
+        insert_command.execute('3456', 'doc type 1',  Time.now - 30.minutes)
+        insert_command.execute('2345', 'doc type 2',  Time.now - 20.minutes)
+
+        expect(DocumentAssignedEvents.fetch_all_new.length).to eql 3
+        expect(DocumentAssignedEvents.fetch_all_new.length).to eql 0
       end
     end
   end
