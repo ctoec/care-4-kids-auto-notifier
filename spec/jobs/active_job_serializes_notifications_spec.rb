b8cdfe6b85f8efe7e42030ecfc8b2e5ce7846702
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2019-08-22 18:21:39 -0400

    Refactor NotificationSendJob test for better clarity and effectiveness

---
+++ spec/jobs/active_job_serializes_notifications_spec.rb
@@ -0,0 +1,85 @@
+require 'rails_helper'
+
+# While this test calls NotificationSendJob, it is more accurately testing the
+# custom serializers within the job pipeline. These tests allow us to test the
+# sustainability of ActiveJob/DelayedJob storage and retrival.
+RSpec.describe NotificationSendJob do
+  include ActiveJob::TestHelper
+
+  context 'when a notification is enqueued for later with real ActiveRecord objects' do
+    it 'executes from the delayed_jobs table' do
+      message_text = 'This is a message'
+      cellphonenumber = '+1234567890'
+      caseid = rand(100).to_s
+      
+      parent = Applicant.create(caseid: caseid, cellphonenumber: cellphonenumber)
+      notification = Notification.create(message_text: message_text)
+      notificationid = notification.id
+
+      notification_event = NotificationEvent.new(caseid, notificationid)
+
+      expect(Sender.count).to eq 0
+      perform_enqueued_jobs {
+        NotificationSendJob.set(wait: 3.seconds).perform_later(Sender, notification_event)
+      }
+      expect(Sender.count).to eq 1
+    end
+  end
+
+  context 'when a notification is enqueued for later with double ActiveRecord objects' do
+    it 'executes from the delayed_jobs table' do
+      message_text = 'This is a message'
+      cellphonenumber = '+1234567890'
+      caseid = rand(100).to_s
+      notificationid = 1
+
+      # Prepare for ActiveRecord doubles
+      ApplicantCache = Applicant
+      NotificationCache = Notification
+      Object.instance_eval { remove_const :Applicant } unless not defined? Applicant
+      Object.instance_eval { remove_const :Notification } unless not defined? Notification
+
+      # Create ActiveRecord doubles
+      parent = double
+      allow(parent).to receive(:cellphonenumber).and_return(cellphonenumber)
+      Applicant = double
+      allow(Applicant).to receive(:find_by).and_return(parent)
+
+      notification = double
+      allow(notification).to receive(:message_text).and_return(message_text)
+      Notification = double
+      allow(Notification).to receive(:find).and_return(notification)
+      
+      notification_event = NotificationEvent.new(caseid, notificationid)
+
+      expect(Sender.count).to eq 0
+      perform_enqueued_jobs {
+        NotificationSendJob.set(wait: 3.seconds).perform_later(Sender, notification_event)
+      }
+      expect(Sender.count).to eq 1
+
+      expect(Applicant).to have_received(:find_by).with(caseid: caseid)
+      expect(Notification).to have_received(:find).with(notificationid)
+
+      # Clean up ActiveRecord doubles
+      Object.instance_eval { remove_const :Applicant } unless not defined? Applicant
+      Object.instance_eval { remove_const :Notification } unless not defined? Notification
+      Applicant = ApplicantCache
+      Notification = NotificationCache
+    end
+  end
+end
+
+class Sender
+  @@count = 0
+
+  def self.createMessage(*varargs)
+    @@count += 1
+  end
+
+  def self.count
+    cached_count = @@count
+    @@count = 0
+    cached_count
+  end
+end
--- spec/jobs/notification_send_job_spec.rb
+++ spec/jobs/notification_send_job_spec.rb
@@ -1,76 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe NotificationSendJob, :type => :job do
-  include ActiveJob::TestHelper
-
-  message_text = 'This is a message'
-  to_number = '+1234567890'
-
-  before(:all) do
-    ApplicantCache = Applicant
-    NotificationCache = Notification
-  end
-
-  before(:each) do
-    Object.instance_eval { remove_const :Applicant } unless not defined? Applicant
-    Object.instance_eval { remove_const :Notification } unless not defined? Notification
-
-    parent = double
-    allow(parent).to receive(:cellphonenumber).and_return(to_number)
-    Applicant = double
-    allow(Applicant).to receive(:find_by).and_return(parent)
-
-    notification = double
-    allow(notification).to receive(:message_text).and_return(message_text)
-    Notification = double
-    allow(Notification).to receive(:find).and_return(notification)
-  end
-  
-  after(:each) do
-    Object.instance_eval { remove_const :Applicant } unless not defined? Applicant
-    Object.instance_eval { remove_const :Notification } unless not defined? Notification
-  end
-
-  after(:all) do
-    Applicant = ApplicantCache
-    Notification = NotificationCache
-  end
-
   context 'when a notification event is supplied' do  
     it '.perform is called with the corresponding message and cell phone numbers' do
+      message_text = 'This is a message'
+      cellphonenumber = '+1234567890'
+      caseid = rand(100).to_s
+
       sender = double
       allow(sender).to receive(:createMessage)
 
-      notification_event = spy
+      parent = Applicant.create(caseid: caseid, cellphonenumber: cellphonenumber)
+      notification = Notification.create(message_text: message_text)
+
+      notification_event = NotificationEvent.new(caseid, notification.id)
 
       NotificationSendJob.perform_now(sender, notification_event)
 
       expect(sender).to have_received(:createMessage).exactly(:once)
-      expect(sender).to have_received(:createMessage).with(message_text: message_text, to_number: to_number)
+      expect(sender).to have_received(:createMessage).with(message_text: message_text, to_number: cellphonenumber)
     end
   end
 
-  context 'when a notification is enqueued for later' do
-    it 'executes from the delayed_jobs table' do
-      notification_event = NotificationEvent.new("123", 1) # caseid, notificationid
-
-      expect(Sender.count).to eq 0
-      perform_enqueued_jobs {
-        NotificationSendJob.set(wait: 3.seconds).perform_later(Sender, notification_event)
-      }
-      expect(Sender.count).to eq 1
-    end
-  end
-end
-
-class Sender
-  @@count = 0
-
-  def self.createMessage(*varargs)
-    @@count += 1
-  end
-
-  def self.count
-    @@count
-  end
+  
 end
