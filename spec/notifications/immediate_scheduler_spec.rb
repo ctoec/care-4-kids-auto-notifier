0d29d85be2b469510516ddf7ef5e06f074a6784a
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2019-08-27 08:23:05 -0400

    Make ImmediateScheduler tests for robust

--- spec/notifications/immediate_scheduler_spec.rb
+++ spec/notifications/immediate_scheduler_spec.rb
@@ -9,4 +9,28 @@ RSpec.describe ImmediateScheduler do
 
     expect(job).to have_received(:perform_now).exactly(:once)
   end
+
+  context 'when one parameter is supplied to scheduler' do
+    it '.schedule calls perform_now on a job with supplied parameter' do
+      job = double
+      allow(job).to receive(:perform_now)
+
+      ImmediateScheduler.schedule(job, 'parameter')
+
+      expect(job).to have_received(:perform_now).exactly(:once)
+      expect(job).to have_received(:perform_now).with('parameter')
+    end
+  end
+
+  context 'when many parameter are supplied to scheduler' do
+    it '.schedule calls perform_now on a job with supplied parameter' do
+      job = double
+      allow(job).to receive(:perform_now)
+
+      ImmediateScheduler.schedule(job, 'parameter 1', 2, nil)
+
+      expect(job).to have_received(:perform_now).exactly(:once)
+      expect(job).to have_received(:perform_now).with('parameter 1', 2, nil)
+    end
+  end
 end
