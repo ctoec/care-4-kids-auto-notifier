3dc9fec3d59b9ffe2632859b8ea0be0cd2b59865
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2019-08-29 16:52:13 -0400

    Implement WakingHoursScheduler

--- app/notifications/waking_hours_scheduler.rb
+++ app/notifications/waking_hours_scheduler.rb
@@ -1,19 +1,33 @@
 # frozen_string_literal: true
 
 class WakingHoursScheduler
+  @@EarliestTime = 8
+  @@LatestTime = 21
+
   def self.schedule(job, *objs)
-    job.perform_now(*objs)
+    if isTooEarly || isTooLate then
+      job.set(wait_until: getNextScheduleTime).perform_later(*objs)
+    else
+      job.perform_now(*objs)
+    end
   end
 
   def self.getNextScheduleTime
-
+    now = Time.now
+    if isTooEarly then
+      Time.new(now.year, now.month, now.day, @@EarliestTime, 0, 0)
+    elsif isTooLate
+      Time.new(now.year, now.month, now.day + 1, @@EarliestTime, 0, 0)
+    else
+      now
+    end
   end
 
   def self.isTooEarly
-
+    Time.now.hour < @@EarliestTime
   end
 
   def self.isTooLate
-
+    Time.now.hour >= @@LatestTime
   end
 end
