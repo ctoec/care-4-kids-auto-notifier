6f40a0b22939940ddb23f0e87f88a6fa3a9c05a9
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2019-09-09 14:30:20 -0400

    Remove init guard and update method name

--- twilio-studio/assets/web-scraper.private.js
+++ twilio-studio/assets/web-scraper.private.js
@@ -8,30 +8,22 @@ class WebScraper {
 
   async init() {
     const response = await this.fetch();
-    this.$ = this.processResponse(response);
+    this.$ = this.loadSite(response);
   }
 
   async fetch() {
     return await axios.get(this.siteUrl);
   };
 
-  processResponse(response) {
+  loadSite(response) {
     return cheerio.load(response.data);
   }
 
-  initGuard() {
-    if (!this.$) {
-      throw new Error("Please run init() before calling this method.");
-    }
-  }
-
   getRedeterminationsProcessingStatusDates() {
-    this.initGuard();
     return this.$('main div ul li:nth-child(2) b span span').text().trim();
   }
 
   getSupportingDocumentsProcessingStatusDates() {
-    this.initGuard();
     return this.$('main div ul li:nth-child(3) b span span').text().trim();
   }
 }
