3f4ace1fc362374c5bf990a7c18f4865f465044e
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2019-09-13 07:52:40 -0400

    Implement failed jobs tracking in NotificationSendJob

--- app/jobs/notification_send_job.rb
+++ app/jobs/notification_send_job.rb
@@ -3,6 +3,24 @@
 class NotificationSendJob < ApplicationJob
   queue_as :default
 
+  retry_on StandardError, attempts: 1 do |job, error|
+    notification_event = job.arguments[1]
+    caseid = notification_event.caseid
+    notificationid = notification_event.notificationid
+    parent = Parent.find_by!(caseid: caseid)
+    
+    is_success = FailedJob.create(jobid: job.job_id, notification_id: notificationid, parent_id: parent.id)
+    Rails.logger.error( 
+      "Could not create failed job record. Notification ID: #{notificationid}, Case ID: #{caseid}"
+    ) unless is_success
+
+    rescue ActiveRecord::RecordNotFound
+      Rails.logger.error "Could not find parent with Case ID: #{caseid}"
+    rescue => e
+      Rails.logger.error e
+      raise e
+  end
+
   def perform(sender, notification_event)
     parent = Parent.find_by(caseid: notification_event.caseid)
     notification = Notification.find(notification_event.notificationid)
