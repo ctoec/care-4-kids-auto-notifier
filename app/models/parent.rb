54ffc887d4caed029448831fca5d235d76ce0f24
Melanie <melanie.mazanec@gmail.com>
2019-09-16 11:07:14 -0400

    Add validations to parent model

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class Parent < ApplicationRecord
+  validates :cellphonenumber, format: { with: /[0-9]{10}/, message: "Please enter a ten digit phone number in the format xxxxxxxxxx" }
+  validates :active, inclusion: [true, false]
+  # validates :caseid, format: { }
+
   @@CELLPHONENUMBER_ENCRYPTION_KEY = Base64.decode64 ENV.fetch 'CELLPHONENUMBER_ENCRYPTION_KEY'
 
   attr_encrypted_options.merge!(marshal: true, allow_empty_value: true)
